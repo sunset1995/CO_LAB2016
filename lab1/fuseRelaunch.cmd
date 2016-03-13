ceeca665b5e6ca084ed20b3e77e36bdc41bef745
Sun mokoto <s2821d3721@gmail.com>
2016-03-13 20:39:51 +0800

    刪除 lab1

--- lab1/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/oiu/Desktop/computer-organization/lab1/testbench_isim_beh.exe" -prj "C:/Users/oiu/Desktop/computer-organization/lab1/testbench_beh.prj" "work.testbench" "work.glbl" 
