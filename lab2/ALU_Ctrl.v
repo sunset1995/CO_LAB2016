e5bd46a0e0da129a3303752b48d051eeda373f21
Sun makoto <s2821d3721@gmail.com>
2016-04-20 17:26:10 +0800

    Complete# alu control

--- lab2/ALU_Ctrl.v
+++ lab2/ALU_Ctrl.v
@@ -24,10 +24,20 @@ output     [4-1:0] ALUCtrl_o;
 //Internal Signals
 reg        [4-1:0] ALUCtrl_o;
 
-//Parameter
-
-       
 //Select exact operation
+always @(*) begin
+	case(funct_i) begin
+		32: ALUCtrl_o <= 0'b0010;
+		34: ALUCtrl_o <= 0'b0110;
+		36: ALUCtrl_o <= 0'b0000;
+		37: ALUCtrl_o <= 0'b0001;
+		42: ALUCtrl_o <= 0'b0111;
+		default: begin
+			ALUCtrl_o[3]   <= 0;
+			ALUCtrl_o[2:0] <= ALUOp_i[2:0];
+		end
+	end
+end
 
 endmodule     
 
