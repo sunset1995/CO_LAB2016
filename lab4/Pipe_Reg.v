d4a52000d4a7dbe2df03cb5c4893768c75dc0376
Sun makoto <s2821d3721@gmail.com>
2016-05-25 20:14:54 +0800

    Update# Pipe_Reg

--- lab4/Pipe_Reg.v
+++ lab4/Pipe_Reg.v
@@ -10,23 +10,23 @@
 //--------------------------------------------------------------------------------
 module Pipe_Reg(
     clk_i,
-	rst_i,
-	data_i,
-	data_o
-	);
+    rst_i,
+    data_i,
+    data_o
+    );
 
 parameter size = 0;
 
-input					clk_i;		  
-input					rst_i;
-input		[size-1: 0]	data_i;
-output reg	[size-1: 0]	data_o;
+input                   clk_i;        
+input                   rst_i;
+input       [size-1: 0] data_i;
+output reg  [size-1: 0] data_o;
 
-always @(posedge clk_i) begin
+always @(posedge clk_i or negedge rst_i) begin
     if(~rst_i)
         data_o <= 0;
     else
         data_o <= data_i;
 end
 
-endmodule	
+endmodule   
