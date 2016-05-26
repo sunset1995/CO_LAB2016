f83f575a1785fa5264d5d755bcf900cf49d06265
Sun makoto <s2821d3721@gmail.com>
2016-05-26 15:54:19 +0800

    Fix# Testbench

--- lab4/TestBench.v
+++ lab4/TestBench.v
@@ -38,14 +38,14 @@ initial  begin
     count = 0;
     
     #(`CYCLE_TIME)      RST = 1;
-    #(`CYCLE_TIME*1500) $stop;
+    #(`CYCLE_TIME*1200) $stop;
     //#(`CYCLE_TIME*20) $fclose(handle); $stop;
 end
 
 //Print result to "CO_P4_Result.dat"
 always@(posedge CLK) begin
     count = count + 1;
-    if( count == 1500 ) begin 
+    if( count == 1100 ) begin 
     //print result to transcript 
     $display("Register===========================================================\n");
     $display("r0=%d, r1=%d, r2=%d, r3=%d, r4=%d, r5=%d, r6=%d, r7=%d\n",
