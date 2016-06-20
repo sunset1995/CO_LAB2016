2a9fbe6ec7dc77e9f045b388ea2c02eda376f898
Sunset <s2821d3721@gmail.com>
2016-06-20 11:56:24 +0800

    Debuggin.cpp show more info

--- lab5/debuggin.cpp
+++ lab5/debuggin.cpp
@@ -31,6 +31,9 @@ void simulate(int cache_size, int block_size) {
 	int offset_bit = log_2(block_size);
 	int index_mask = (1<<index_bit)-1;
 	vector<cache_content> cache(line);
+	vector<bool> neverTrue(line, true);
+	vector<int>  accessCnt(line, 0);
+
 
 	printf("cache size: %d\n", cache_size);
 	printf("block size: %d\n", block_size);
@@ -51,19 +54,31 @@ void simulate(int cache_size, int block_size) {
 		printf("addr %06x ; nodt %06x ; tag %3x ; index %3x ; %s\n", addr, nodt, tag, index, (cache[index].v==false)? "Miss(first)" : (cache[index].tag==tag? "Hit" : "Miss"));
 
 		++total;
-		if( cache[index].v && cache[index].tag==tag )
+		if( cache[index].v && cache[index].tag==tag ) {
 			++hit;
+			neverTrue[index] = false;
+		}
 		else {
 			cache[index].v   = true;
 			cache[index].tag = tag;
 		}
+		++accessCnt[index];
 	}
 	fclose(fp);
-	
+
+	for(int i=0; i<line; ++i)
+		printf("%d%s ", accessCnt[i], neverTrue[i]? "x" : "");
+	puts("");
+
+	int sumNeverTrue = 0;
+	for(int i=0; i<line; ++i)
+		sumNeverTrue += neverTrue[i];
+	printf("never true: %d/%d = %.2f%%\n", sumNeverTrue, line, 100.0*sumNeverTrue/line);
+
 	printf("miss rate: %d/%d = %.2f%%\n", (total-hit), total, 100.0*(total-hit)/total);
 }
 	
 int main() {
 	// Let us simulate 4KB cache with 16B blocks
-	simulate(64, 4);
+	simulate(128, 4);
 }
