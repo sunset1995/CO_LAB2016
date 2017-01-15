73b354f16f8e78299b262c46ef0c5eb2e93fed31
ChiWeiHsiao <kiwi010379@gmail.com>
2017-01-15 09:56:32 +0800

    Add gate level adder

---
+++ adder_8bits.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    09:54:11 01/15/2017 
+// Design Name: 
+// Module Name:    adder_8bits 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module adder_8bits(
+	input  [7:0] in1,
+	input  [7:0] in2,
+	output [7:0] sum,
+	output       c
+	);
+
+
+endmodule
---
+++ full_adder.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    09:53:05 01/15/2017 
+// Design Name: 
+// Module Name:    full_adder 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module full_adder(
+	input a,
+	input b,
+	input cIn,
+	output s,
+	output cOut
+	);
+
+
+endmodule
